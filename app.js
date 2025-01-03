6b817cdae134031caa01aa16ff22309a733f6775
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2025-01-03 15:04:24 +0600

    port from env

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+
+const env = require("./config/env");
+const port = env.PORT || 3000;
+
 const routes = require("./routes");
 const errorHandler = require('./utils/errorHandler');
 
--- config/env.js
+++ config/env.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   DB_NAME: process.env.DB_NAME,
-  CHANNEL_COLLECTION: process.env.CHANNEL_COLLECTION
+  CHANNEL_COLLECTION: process.env.CHANNEL_COLLECTION,
+  PORT: process.env.PORT
 };
